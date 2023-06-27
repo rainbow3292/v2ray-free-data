484dde986f217d4aeddd2be589702e35c61a43e0
rainbow3292 <90330568+rainbow3292@users.noreply.github.com>
2023-06-28 00:59:41 +0700

    Update thiet-lap-v2ray.sh

--- thiet-lap-v2ray.sh
+++ thiet-lap-v2ray.sh
@@ -35,16 +35,16 @@ elif [ $dist = "Ubuntu" -o $dist = "Debian" ] ; then
         clear
 fi
 echo "Đang khởi tạo server V2ray"
-sudo docker run --name v2ray -d -p 80:8081 thuonghai2711/v2ray-azure-web:latest
+sudo docker run --name v2ray -d -p 443:8081 thuonghai2711/v2ray-azure-web:latest
 clear
 IP=$(curl -s ifconfig.me)
-echo "Đang kiểm tra kết nối tới v2ray server (đảm bảo VPS đã mở port 80)"
+echo "Đang kiểm tra kết nối tới v2ray server (đảm bảo VPS đã mở port 443)"
 sleep 2
-echo -n "- Đang khởi tạo kết nối: " && if curl -s --connect-timeout 1 $IP | grep -q Bad; then echo -e "$(tput setaf 2)ONLINE - Ping thành công đến IP$(tput sgr0)"; else echo -e "$(tput setaf 1)OFFLINE - Không thể Ping đến IP, vui lòng mở port 80$(tput sgr0)"; fi
+echo -n "- Đang khởi tạo kết nối: " && if curl -s --connect-timeout 1 $IP | grep -q Bad; then echo -e "$(tput setaf 2)ONLINE - Ping thành công đến IP$(tput sgr0)"; else echo -e "$(tput setaf 1)OFFLINE - Không thể Ping đến IP, vui lòng mở port 443$(tput sgr0)"; fi
 echo "Thành công, sau đây là thông tin v2ray, vui lòng sử dụng nó để điền vào ứng dụng v2ray"
 echo "Address (Địa chỉ): $IP"
-echo "Port (Cổng): 80"
-echo "UUID(id) (Địa chỉ ID): ba651c8c-cb35-4c46-bf6c-f90bd6f094e3"
+echo "Port (Cổng): 443"
+echo "UUID(id) (Địa chỉ ID): ba651c8c-cb35-4c46-bf6c-f90bd6f094e4"
 echo "AlterID: 64"
 echo "Protocol (Kiểu kết nối): WS"
 echo "Request host (Yêu cầu host): thay thế 1 trong 2 địa chỉ sau"
